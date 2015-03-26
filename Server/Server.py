973c86613d39709d617e4e2cdee82849d1777f78
Ole Andreas Hansen <oleaha@me.com>
2015-03-26 12:08:01 +0100

    Done deal!

--- Server/Server.py
+++ Server/Server.py
@@ -2,23 +2,34 @@
 import SocketServer
 import json
 import datetime
+import time
 
 connected_clients = []
 online_usernames = {}
 
+messages = []
+
+
+# Helper methods #
+def login(username):
+    if username in online_usernames.values():
+        return False
+    return True
+
+
+def get_datetime():
+    return str(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
+
+
+class Bcolors:
+    SYSTEM = '\033[91m'
+    USER = '\033[92m'
+    ENDC = '\033[0m'
+
 
 class ClientHandler(SocketServer.BaseRequestHandler):
-    """
-    This is the ClientHandler class. Everytime a new client connects to the
-    server, a new ClientHandler object will be created. This class represents
-    only connected clients, and not the server itself. If you want to write
-    logic for the server, you must write it outside this class
-    """
 
     def handle(self):
-        """
-        This method handles the connection between a client and the server.
-        """
         self.ip = self.client_address[0]
         self.port = self.client_address[1]
         self.connection = self.request
@@ -34,48 +45,65 @@ class ClientHandler(SocketServer.BaseRequestHandler):
             data = json.loads(received_string)
 
             if data['request'] == 'login':
-                if self.login(data['username']):
-                    online_usernames[self.port] = data['username']
-                    self.make_package('login', data['username'])
-                    self.make_package('message', self.get_datetime() + ' - system : ' + data['username'] + ' joined the channel')
-                else:
-                    self.make_package('error', 'Username already exists')
+                self.handle_login(data)
             elif data['request'] == 'logout':
-                if not self.login(data['username']):
-                    self.make_package('logout', self.get_datetime() + ' - system : ' + data['username'] + ' left the channel')
-                    del online_usernames[self.port]
-                    print 'Client disconnected! ' + self.ip + ':' + str(self.port)
-                    connected_clients.remove(self)
+                self.handle_logout(data)
             elif data['request'] == 'users':
-                users = self.get_datetime() + ' - system : online users - '
-                for i in online_usernames:
-                    users += online_usernames[i] + ', '
-                self.make_package('users', users)
-
+                self.handle_users()
             elif data['request'] == 'message':
-                message = self.get_datetime() + ' - ' + data['username'] + ': ' + data['message']
-                self.make_package('message', message)
+                self.handle_message(data)
+
+    # Validate login and send response
+    def handle_login(self, data):
+        # Make sure that the user is NOT logged in
+        if login(data['username']):
+            online_usernames[self.port] = data['username']
+            self.make_package('login', data['username'])
+            time.sleep(0.1)
+            self.connection.sendall(json.dumps({'response': 'history', 'message': messages}))
+            time.sleep(0.1)
+            message = Bcolors.SYSTEM + get_datetime() + ' - system : ' + data['username'] + ' joined the channel' + Bcolors.ENDC
+            self.make_package('message', message)
+            messages.append(message)
+
+        else:
+            self.make_package('error', Bcolors.SYSTEM + '##FAIL: Username already exists ##' + Bcolors.ENDC)
+
+    # Handle logout the correct way
+    def handle_logout(self, data):
+        # Make sure the client is logged in
+        if not login(data['username']):
+            # Broadcast logout message
+            message = Bcolors.SYSTEM + get_datetime() + ' - system : ' + data['username'] + ' left the channel' + Bcolors.ENDC
+            self.make_package('logout', message)
+            messages.append(message)
+            # Delete client from online users list
+            del online_usernames[self.port]
+            print 'Client disconnected! ' + self.ip + ':' + str(self.port)
+            # Remove client from connected clients to avoid unexpected shutdown
+            connected_clients.remove(self)
+
+    # Handle the list of online users
+    def handle_users(self):
+        users = Bcolors.SYSTEM + get_datetime() + ' - system : online users - '
+        for i in online_usernames:
+            users += online_usernames[i] + ', '
+        users += Bcolors.ENDC
+        self.make_package('users', users)
+
+    # Handle incoming messages
+    def handle_message(self, data):
+        message = get_datetime() + ' - ' + data['username'] + ': ' + data['message']
+        self.make_package('message', message)
+        messages.append(message)
 
     def make_package(self, response, message):
         self.broadcast(json.dumps({'response': response, 'message': message}))
-
 
     #When a new message is added, send this to all clients
     def broadcast(self, response):
         for client in connected_clients:
             client.request.sendall(response)
-
-    @staticmethod
-    def login(username):
-        if username in online_usernames.values():
-            return False
-        return True
-
-    @staticmethod
-    def get_datetime():
-        return str(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
-
-
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
